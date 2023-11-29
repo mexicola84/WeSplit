03ddf8678c5b34ab8097cbcc83285b42cb2a5521
mexicola84 <i@jan-grimm.com>
2023-11-29 21:26:29 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jan Grimm on 29.11.23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
